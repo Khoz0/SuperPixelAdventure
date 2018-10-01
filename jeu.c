c11b35e887ef3427de14fc638b720a177778d07d
Corentin Roberge- Mentec <corentin.roberge-mentec6@etu.univ-lorraine.fr>
2018-10-01 12:48:35 +0200

    Sprint + Stamina + PNJ

--- jeu.c
+++ jeu.c
@@ -5,38 +5,38 @@
 #define LARGEUR_TILE 32  // hauteur et largeur des tiles.
 #define HAUTEUR_TILE 32 
 
-#define NOMBRE_BLOCS_LARGEUR 45  // nombre a afficher en x et y
-#define NOMBRE_BLOCS_HAUTEUR 28
+#define NOMBRE_BLOCS_LARGEUR 45  // nombre a afficher en x et y // 135
+#define NOMBRE_BLOCS_HAUTEUR 28                                 // 84    pour map de 3x3
 
 char* table[] = {
-"555555555555555555555555555555555555555655555",
-"556555555555555555555555555556555555555555555",
-"555555555555555555555555555555555555555555555", 
-"555556555555555555555555555555555555555555555",
-"555555555555555555555555555555555555555555555",
-"555555555555555555555555555555555555555555555",
-"555555555555555555565555555555555555555555555",
-"555555555555555555555555555555555555555555555",
-"555555555555555555555555555555555555555555555",
-"555555555555555555555555555555555555555555555",
-"555555555555555555555555555555555555555555555",
-"555555555555555555555555555555555555555555555",
-"555555655555555555555555555555555555555555555",
-"555555555555555555555555555555555555555555555",
-"555555555555555555555555655555555555655555555",
-"555555555555555555555555555555555555555555555",
-"555555555555555555555555555555555555555555555",
-"555555555555555555555555555555555555555555555",
-"555555555555555555555555555555555555555555555",
-"555555555555555555555555555555555555555555555",
-"555555555555555555555555555555555555555555555",
-"555555555555555555555555555555555556555555555",
-"555555555555555555555555555555555555555555555",
-"555555556555555555555555555555555555555555555",
-"555555555555555555555555555555555555555555555",
-"555555555555555555555555555555555555555555555",
-"555555555555555555555555555555555555555555555",
-"555555555555555555555555555555555555555555555"};
+"555555555555522255555555555555555522255655555",
+"555555555555562225655555555556555522555555555",
+"555555555555555222555555555555555522555555555", 
+"555556555555555522255555555555555522555555555",
+"555555555555555552255555555555555522555555555",
+"555555555555555552255555555555555522555555555",
+"555555555555555552265555555555555522555555555",
+"555555555555555552255555555555555522555555555",
+"555555555555555552255555555555555522555555555",
+"555555555555555552255555555555555522555555555",
+"555555555555555552255555555555555222555555555",
+"555555555555555552255555555555555225555555555",
+"DDDDDDDDDDDDDDDDD225555555555552222DDDDDDDDDD",
+"222222BB2222222222222226222222222222222222222",
+"222222222222222222222222222222222222222622222",
+"DDDDDDDDDDDDDDDDD2255555555555522DDDDDDDDDDDD",
+"555555555555555552255555555555522555555555555",
+"555555555555555552255555555555222555555555555",
+"555555555555555552255555555552225555555555555",
+"555555555555555552255555555552255555555555555",
+"555555555555555552255555555552255555555555555",
+"555555555555555552255555555522255556555555555",
+"555555555555555552255555555222555555555555555",
+"555555556555555552222255552225555555555555555",
+"555555555555555555652255522255555555555555555",
+"555555555555555555552225222555555555555555555",
+"555555555555555555555222225555555555555555555",
+"555555555555555555555552225555555555555555555"};
 
 
 
@@ -101,47 +101,47 @@ void Afficher(SDL_Surface* screen,SDL_Surface* tileset,char** table,int nombre_b
 
 int main(int argc,char** argv)
 {
-    SDL_Surface *rightChar = NULL;
-    SDL_Rect positionChar, positionMap, rightCharGo;
+    SDL_Surface *mainChar = NULL, *stamina = NULL, *lifePoint = NULL, *wizardWPNJ = NULL;
+    SDL_Rect positionChar, mainCharGo, staminaPos, lifePointPos, positionWizardWPNJ, posSpriteWizardPNJ;
     
     int gameOver = 1;
-    int dir = 0;
+    int dir = 1, width = 2, sprint = 1, staminaLength = 195;
     
     SDL_Surface *screen,*tileset;
     SDL_Event event;
     
-    positionMap.x = 0;
-    positionMap.y = 0;
-    
-    positionChar.x = (1410/2) - (30/2);
+    positionChar.x = 50;
     positionChar.y = (860/2) - (36/2);
+	
+	positionWizardWPNJ.x = 960;
+	positionWizardWPNJ.y = 480;
     
-    SDL_Init(SDL_INIT_VIDEO);		// prepare SDL
+    SDL_Init(SDL_INIT_VIDEO);
     screen = SDL_SetVideoMode(LARGEUR_TILE*NOMBRE_BLOCS_LARGEUR, HAUTEUR_TILE*NOMBRE_BLOCS_HAUTEUR, 32,SDL_HWSURFACE|SDL_DOUBLEBUF);
     
-    rightChar = SDL_LoadBMP("heroDroite.bmp");
+    mainChar = SDL_LoadBMP("hero.bmp");
+	wizardWPNJ = SDL_LoadBMP("wizardWoman.bmp");
     tileset = SDL_LoadBMP("background_jeu.bmp");
     
-    SDL_SetColorKey(rightChar, SDL_SRCCOLORKEY, SDL_MapRGB(rightChar->format, 255, 255, 255));
-    
+	staminaPos.x = 10;
+	staminaPos.y = 45;
+	
+	lifePointPos.x = 10;
+	lifePointPos.y = 20;
+	
     SDL_EnableKeyRepeat(10, 10);
     
     if (!tileset)
     {
-	printf("Echec de chargement background.png\n");
+	printf("Echec de chargement background.bmp\n");
 	SDL_Quit();
 	system("pause");
 	exit(-1);
     }
 	
     Afficher(screen,tileset,table,NOMBRE_BLOCS_LARGEUR,NOMBRE_BLOCS_HAUTEUR);
-    
-    do 
-    {
-	SDL_WaitEvent(&event);
-    }
-    while (event.type!=SDL_KEYDOWN);
-	
+    SDL_SetColorKey(mainChar, SDL_SRCCOLORKEY, SDL_MapRGB(mainChar->format, 255, 255, 255));
+	SDL_SetColorKey(wizardWPNJ, SDL_SRCCOLORKEY, SDL_MapRGB(wizardWPNJ->format, 255, 255, 255));
     
     while (gameOver)
     {
@@ -151,60 +151,143 @@ int main(int argc,char** argv)
           case SDL_KEYDOWN:
               switch(event.key.keysym.sym)
               {
-                case SDLK_UP:
-				if (positionChar.y > 0){ 
-					positionChar.y-=2;
-				}else{
-					positionChar.y += 0;
-				}
-                break;
-                case SDLK_DOWN:
-				if (positionChar.y < 860){
-					positionChar.y += 2;
-				}else{
-					positionChar.y += 0;
-				}
-                break;
-                case SDLK_RIGHT:
-				if (positionChar.x < 1410){
-					positionChar.x += 4;
+			  case SDLK_LSHIFT:
+				sprint = 2;
+			  break;
+              case SDLK_z:
+				width = 0;
+				if (positionChar.y > 0){
+					positionChar.y -= (4 * sprint);
 					if (dir < 20){
-						dir += 1;
+						dir += (1 * sprint);
 					}else{
 						dir = 0;
+					}
+					if (sprint == 2 && staminaLength > 1){
+						staminaLength -= (2 * sprint);
+					}else if (staminaLength <= 2){
+						sprint = 1;
+					}
+				}else{
+					positionChar.y += 0;
+				}
+              break;
+              case SDLK_s:
+				width = 2;
+				if (positionChar.y < 860){
+					positionChar.y += (4 * sprint);
+					if (dir < 20){
+						dir += (1 * sprint);
+					}else{
+						dir = 0;
+					}
+					if (sprint == 2 && staminaLength > 1){
+						staminaLength -= (2 * sprint);
+					}else if (staminaLength <= 2){
+						sprint = 1;
+					}
+				}else{
+					positionChar.y += 0;
+				}
+			  break;
+			  case SDLK_d:
+				width = 1;
+				if (positionChar.x < 1410){
+					positionChar.x += (4 * sprint);
+					if (dir < 20){
+						dir += (1 * sprint);
+					}else{
+						dir = 0;
+					}
+					if (sprint == 2 && staminaLength > 1){
+						staminaLength -= (2 * sprint);
+					}else if (staminaLength <= 2){
+						sprint = 1;
 					}
 				}else{
 					positionChar.x += 0;
 				}
-                break;
-                case SDLK_LEFT:
+              break;
+              case SDLK_q:
+				width = 3;
 				if (positionChar.x > 0){
-					positionChar.x -= 2;
+					positionChar.x -= (4 * sprint);
+					if (dir < 20){
+						dir += (1 * sprint);
+					}else{
+						dir = 0;
+					}
+					if (sprint == 2 && staminaLength > 1){
+						staminaLength -= (2 * sprint);
+					}else if (staminaLength <= 2){
+						sprint = 1;
+					}
 				}else{
-					positionChar.x += 0;
+				  positionChar.x += 0;
 				}
-                break;
-			case SDLK_q:
-				gameOver = 0;
-				break;
-			  }
+              break;
             break;
+		case SDLK_ESCAPE:
+		  gameOver = 0;
+		break;
+	    }
+      break;
+	  case SDL_KEYUP:
+	      switch(event.key.keysym.sym)
+	      {
+		  case SDLK_LSHIFT:
+		    sprint = 1;
+		  break;
+		  case SDLK_z:
+			dir = 8;
+		  break;
+		  case SDLK_s:
+			dir = 8;
+		  break;
+		  case SDLK_d:
+			dir = 8;
+		  break;
+		  case SDLK_q:
+			dir = 8;
+		  break;
+		  }
+	  break;
+      
       }
-      
-      SDL_FillRect(screen, NULL, SDL_MapRGB(screen->format, 0, 0, 0));
-      
-      rightCharGo.x = (30)*(dir/7);
-      rightCharGo.y = 0;
-      rightCharGo.h = 36;
-      rightCharGo.w = 30;
+	  if (staminaLength > -2 && staminaLength <= 194 && sprint == 1){
+		staminaLength += (2 * sprint);
+	  }
+	  
+	  stamina = SDL_CreateRGBSurface(SDL_HWSURFACE, staminaLength + 5, 15, 32, 0, 0 ,0 ,0);
+	  lifePoint = SDL_CreateRGBSurface(SDL_HWSURFACE, 200, 15, 32, 0, 0 ,0 ,0);
+	  
+	  SDL_FillRect(stamina, NULL, SDL_MapRGB(screen->format, 1, 215, 88));
+	  SDL_FillRect(lifePoint, NULL, SDL_MapRGB(screen->format, 200, 7, 7));
+	  
+      mainCharGo.x = 33*(dir/7);
+      mainCharGo.y = 36*width;
+      mainCharGo.h = 36;
+      mainCharGo.w = 30;
+	  
+      posSpriteWizardPNJ.x = 30;
+      posSpriteWizardPNJ.y = 30;
+      posSpriteWizardPNJ.h = 36;
+      posSpriteWizardPNJ.w = 30;
+	
       Afficher(screen,tileset,table,NOMBRE_BLOCS_LARGEUR,NOMBRE_BLOCS_HAUTEUR);
-      SDL_BlitSurface(rightChar, &rightCharGo, screen, &positionChar);
+      SDL_BlitSurface(stamina, NULL, screen, &staminaPos);
+      SDL_BlitSurface(lifePoint, NULL, screen, &lifePointPos);
+      SDL_BlitSurface(wizardWPNJ, &posSpriteWizardPNJ, screen, &positionWizardWPNJ);
+      SDL_BlitSurface(mainChar, &mainCharGo, screen, &positionChar);
       SDL_UpdateRect(screen, 0, 0, 0, 0);
     }
       
     SDL_FreeSurface(tileset);
-    SDL_FreeSurface(rightChar);
+	SDL_FreeSurface(stamina);
+	SDL_FreeSurface(lifePoint);
+	SDL_FreeSurface(wizardWPNJ);
+    SDL_FreeSurface(mainChar);
     SDL_FreeSurface(screen);
-	SDL_Quit();
-	return 0;
+    SDL_Quit();
+    return 0;
 }
