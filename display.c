cde2c6dad369d3cb8fba954216a0dfb384496dc1
colne5u <colne5u@etu.univ-lorraine.fr>
2018-12-10 22:55:06 +0100

    nettoyage : final

--- display.c
+++ display.c
@@ -9,14 +9,14 @@ one to print the character and PNJs, one to print the pannels & chats chatBox
 */
 
 void display(Game* game) {
-  displayMap(getGameTables(game), getScreen(getGameSdl(game)), getVariable(getGameVariables(game), XSCROLL), getVariable(getGameVariables(game), YSCROLL), getGameAtlas(game));
-  displayPannel(getScreen(getGameSdl(game)), getGameAtlas(game), getGameVariables(game), game);
-  displayChar(getScreen(getGameSdl(game)), getGameAtlas(game));
+  displayMap(game);
+  displayPannel(game);
+  displayChar(game);
   displayWaterfall(game);
   displayBars(game);
 }
 
-void displayMap(Tables* tables, SDL_Surface* screen, int xscroll, int yscroll, Atlas* atlas) {
+void displayMap(Game* game) {
 
   SDL_Rect Rect_dest;
   SDL_Rect Rect_source;
@@ -27,48 +27,48 @@ void displayMap(Tables* tables, SDL_Surface* screen, int xscroll, int yscroll, A
   // we only print the blocks inside of the scroll window
   for(int i = 0 ; i < MAP_BLOCKS_WIDTH ; i++){
     for(int j = 0 ; j < MAP_BLOCKS_HEIGHT ; j++){
-      if((i*WIDTH_TILE > xscroll - WIDTH_TILE) && (i*WIDTH_TILE < xscroll + SCREEN_WIDTH + WIDTH_TILE)
-        && (j*WIDTH_TILE > yscroll - WIDTH_TILE) && (j*WIDTH_TILE < yscroll + SCREEN_HEIGHT + WIDTH_TILE)){
-        if(getTable(tables, MAP_BUILDER)[i][j]<171){
-          Rect_dest.x = i*WIDTH_TILE - xscroll;
-          Rect_dest.y = j*HEIGHT_TILE - yscroll;
-          Rect_source.x = (getTable(tables, MAP_BUILDER)[i][j])*WIDTH_TILE;
+      if((i*WIDTH_TILE > getVariable(getGameVariables(game), XSCROLL) - WIDTH_TILE) && (i*WIDTH_TILE < getVariable(getGameVariables(game), XSCROLL) + SCREEN_WIDTH + WIDTH_TILE)
+        && (j*WIDTH_TILE > getVariable(getGameVariables(game), YSCROLL) - WIDTH_TILE) && (j*WIDTH_TILE < getVariable(getGameVariables(game), YSCROLL) + SCREEN_HEIGHT + WIDTH_TILE)){
+        if(getTable(getGameTables(game), MAP_BUILDER)[i][j]<171){
+          Rect_dest.x = i*WIDTH_TILE - getVariable(getGameVariables(game), XSCROLL);
+          Rect_dest.y = j*HEIGHT_TILE - getVariable(getGameVariables(game), YSCROLL);
+          Rect_source.x = (getTable(getGameTables(game), MAP_BUILDER)[i][j])*WIDTH_TILE;
           Rect_source.y = 0;
-          SDL_BlitSurface(getTileset(atlas, TILESET1), &Rect_source, screen, &Rect_dest);
-        }else if((getTable(tables, MAP_BUILDER)[i][j]<341) && (getTable(tables, MAP_BUILDER)[i][j]>170)){
-          Rect_dest.x = i*WIDTH_TILE - xscroll;
-          Rect_dest.y = j*HEIGHT_TILE - yscroll;
-          Rect_source.x = (getTable(tables, MAP_BUILDER)[i][j]%171)*WIDTH_TILE;
+          SDL_BlitSurface(getTileset(getGameAtlas(game), TILESET1), &Rect_source, getScreen(getGameSdl(game)), &Rect_dest);
+        }else if((getTable(getGameTables(game), MAP_BUILDER)[i][j]<341) && (getTable(getGameTables(game), MAP_BUILDER)[i][j]>170)){
+          Rect_dest.x = i*WIDTH_TILE - getVariable(getGameVariables(game), XSCROLL);
+          Rect_dest.y = j*HEIGHT_TILE - getVariable(getGameVariables(game), YSCROLL);
+          Rect_source.x = (getTable(getGameTables(game), MAP_BUILDER)[i][j]%171)*WIDTH_TILE;
           Rect_source.y = 0;
-          SDL_BlitSurface(getTileset(atlas, TILESET2), &Rect_source, screen, &Rect_dest);
+          SDL_BlitSurface(getTileset(getGameAtlas(game), TILESET2), &Rect_source, getScreen(getGameSdl(game)), &Rect_dest);
         }else{
-          Rect_dest.x = i*WIDTH_TILE - xscroll;
-          Rect_dest.y = j*HEIGHT_TILE - yscroll;
-          Rect_source.x = (getTable(tables, MAP_BUILDER)[i][j]%171)*WIDTH_TILE;
+          Rect_dest.x = i*WIDTH_TILE - getVariable(getGameVariables(game), XSCROLL);
+          Rect_dest.y = j*HEIGHT_TILE - getVariable(getGameVariables(game), YSCROLL);
+          Rect_source.x = (getTable(getGameTables(game), MAP_BUILDER)[i][j]%171)*WIDTH_TILE;
           Rect_source.y = 0;
-          SDL_BlitSurface(getTileset(atlas, TILESET3), &Rect_source, screen, &Rect_dest);
+          SDL_BlitSurface(getTileset(getGameAtlas(game), TILESET3), &Rect_source, getScreen(getGameSdl(game)), &Rect_dest);
         }
       }
     }
   }
-  SDL_FreeSurface(screen);
+  SDL_FreeSurface(getScreen(getGameSdl(game)));
 }
 
-void displayPannel(SDL_Surface* screen, Atlas* atlas, Variables* variables, Game* game) {
-  if(getBoolean(variables, BOOL_PANNEL)) SDL_BlitSurface(getPicture(atlas, PANNEL)->surface, NULL, screen, &getPicture(atlas, PANNEL)->dst);
+void displayPannel(Game* game) {
+  if(getBoolean(getGameVariables(game), BOOL_PANNEL)) SDL_BlitSurface(getPicture(getGameAtlas(game), PANNEL)->surface, NULL, getScreen(getGameSdl(game)), &getPicture(getGameAtlas(game), PANNEL)->dst);
   displayTextPannel(game);
 }
 
-void displayChar(SDL_Surface* screen, Atlas* atlas) {
+void displayChar(Game* game) {
 
   for(int i = 0 ; i < SIZE_ATLAS ; i++) {
-    if(getIsPrinted(atlas, i) && i!=3 && i!=12) {
+    if(getIsPrinted(getGameAtlas(game), i) && i!=3 && i!=12) {
       // we ignore i=3 and i=12 because it's the index of waterfall & pannel, special printings
       // theses conditions are used to make a difference between the last parameter of fonction SDL_BlitzSurface
-      if(!getIsNeg(atlas, i)) {
-        SDL_BlitSurface(getPicture(atlas, i)->surface, &getPicture(atlas, i)->src, screen, &getPicture(atlas, i)->dst);
+      if(!getIsNeg(getGameAtlas(game), i)) {
+        SDL_BlitSurface(getPicture(getGameAtlas(game), i)->surface, &getPicture(getGameAtlas(game), i)->src, getScreen(getGameSdl(game)), &getPicture(getGameAtlas(game), i)->dst);
       }else{
-        SDL_BlitSurface(getPicture(atlas, i)->surface, &getPicture(atlas, i)->src, screen, &getPicture(atlas, i)->neg);
+        SDL_BlitSurface(getPicture(getGameAtlas(game), i)->surface, &getPicture(getGameAtlas(game), i)->src, getScreen(getGameSdl(game)), &getPicture(getGameAtlas(game), i)->neg);
       }
     }
   }
--- display.h
+++ display.h
@@ -9,9 +9,9 @@ typedef struct SDL SDL;
 typedef struct Game Game;
 
 void display(Game* game);
-void displayMap(Tables* tables, SDL_Surface *screen, int xscroll, int yscroll, Atlas* atlas);
-void displayPannel(SDL_Surface* screen, Atlas* atlas, Variables* variables, Game* game);
+void displayMap(Game* game);
+void displayPannel(Game* game);
 void displayTextPannel(Game* game);
-void displayChar(SDL_Surface* screen, Atlas* atlas);
+void displayChar(Game* game);
 void displayWaterfall(Game* game);
 void displayBars(Game* game);
